3a95c6adc77aedb67efa832d060b4e00a3031e28
Elton-Prazeres <elton_hime20@hotmail.com>
2025-03-30 15:58:20 -0300

    ajustado comentado as funcionalidades do JS nas linhas 3,7,15,24

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
 function inserir(num) {
     document.querySelector('.tela').innerHTML += num;
-}
+}// função para capturar o numero digitado
 
 function clean() {
     document.querySelector(".tela").innerHTML = "";
-}
+}//função de limpar o screen da calculadora
 
 function back() {
     let tela = document.querySelector('.tela').innerHTML;
@@ -12,7 +12,7 @@ function back() {
         0,
         tela.length - 1
     );
-}
+}//função limpar o valor em length-1
 
 function somar () {
     let tela = document.querySelector('.tela').innerHTML;
@@ -21,4 +21,4 @@ function somar () {
     } else {
         document.querySelector('.tela').innerHTML = "nenhum comando..."
     }
-}
+}//função somar e operar por eval(tela)
