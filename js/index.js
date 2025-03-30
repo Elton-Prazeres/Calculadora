2a614af93b24f1cd173fab9f13a224a1a5e57d3d
Elton-Prazeres <elton_hime20@hotmail.com>
2025-03-30 10:44:08 -0300

    adicionado função de voltar 'back' os caracteres escritos linhas 8-15

--- js/index.js
+++ js/index.js
@@ -2,6 +2,14 @@ function inserir(num) {
     document.querySelector('.tela').innerHTML += num;
 }
 
-function Clean() {
+function clean() {
     document.querySelector(".tela").innerHTML = "";
+}
+
+function back() {
+    let tela = document.querySelector('.tela').innerHTML;
+    document.querySelector('.tela').innerHTML = tela.substring(
+        0,
+        tela.length - 1
+    );
 }
